5302691779e9697cf9cda9425f5f6de291d3360c
wccXpfhc <89910124+MesakAnggaW@users.noreply.github.com>
2022-12-17 07:20:20 +0700

    Mengerjakan tugas 7 Javascript

--- tugas7.js
+++ tugas7.js
@@ -1,1 +1,12 @@
+let dataTinggiBadan = [190, 180, 170, 160, 150, 140, 130, 120, 110, 100]
 
+for (let index = 0; index < dataTinggiBadan.length; index++) {
+    console.log(dataTinggiBadan[index]);
+
+}
+
+console.log("-------------");
+
+for (const x of dataTinggiBadan) {
+    console.log(x);
+}
